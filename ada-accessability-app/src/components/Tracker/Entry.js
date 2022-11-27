29016b8d04f24ebb5e5fd73c3ea29c7d00d78ed2
Tedi Lowney <tmlowney1@gmail.com>
2022-11-27 13:38:29 -0500

    entry

--- ada-accessability-app/src/components/Tracker/Entry.js
+++ ada-accessability-app/src/components/Tracker/Entry.js
@@ -2,6 +2,7 @@ const Entry = ({ entry }) => {
   return (
     <div className="entry">
       {/* <p>{entry.rating}</p> */}
+      {/* <p>{entry.title}</p> */}
       <p className="smile">&#9786;</p>
       <p>{entry.title}</p>
     </div>
